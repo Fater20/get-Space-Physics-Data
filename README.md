9925f64f9fa85aafa6316c82bdaa3a4b53b28513
yhmac <yanyuheng20@gmail.com>
2025-05-07 00:57:37 +0800

    add get solar wind data

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Space Physics Data Fetcher (Updated 2025.04.28)
+# Space Physics Data Fetcher (Updated 2025.05.07)
 本项目用于快速获取下载各类空间科学数据。
 
 
@@ -6,7 +6,7 @@
 
 ### Geomagnetic Data 
 
-该部分用于从日本京都大学地磁观测中心（WDC for Geomagnetism, Kyoto）自动爬取 **Dst 指数** 和 **AE 指数** 数据,从亥姆霍兹德国地理研究中心（GFZ）自动爬取 **Kp 指数** 和 **ap 指数** 数据。支持按月查询，输出为标准的 `DataFrame` 并可保存为 `.csv` 文件。(更多指数支持更新中...)
+该部分用于从日本京都大学地磁观测中心（WDC for Geomagnetism, Kyoto）自动获取 **Dst 指数** 和 **AE 指数** 数据,从亥姆霍兹德国地理研究中心（GFZ）自动获取 **Kp 指数** 和 **ap 指数** 数据。支持按月查询，输出为标准的 `DataFrame` 并可保存为 `.csv` 文件。(更多指数支持更新中...)
 
 - **获取 Dst 指数**（1小时分辨率）
   - 根据年份自动选择最终版、临时版或实时版数据源。
@@ -19,6 +19,10 @@
 - **统一输出格式**
   - 返回 `pandas.DataFrame` 格式，包含时间戳与对应指数值。
 
+### Solar Wind Data
+
+该部分用于从NASA提供的ftp链接中获取太阳风相关任务的数据。支持按月查询，支持指定任务和时间分辨率，输出为标准的 `DataFrame` 并可保存为 `.csv` 文件。(更多任务支持更新中...)
+
 ## 🌆 安装依赖
 
 ```bash
@@ -27,17 +31,12 @@ pip install -r requirements.txt
 
 ## 📂 文件结构
 
-- `getGeomagneticData.py` — 主程序文件，包含所有核心函数。
+- `getGeomagneticData.py` — 包含地磁相关数据(**Dst**, **AE**, **Kp**, **ap**指数)获取函数。
+- `getSolarwindData.py` — 包含太阳风相关数据(**ACE**任务)获取函数。
 
 ## 🚀 使用说明
 
-### 1. 导入模块
-
-```python
-from getGeomagneticData import getGeomagneticData
-```
-
-### 2. 快速调用示例
+### 1. 快速调用示例
 
 ```python
 # 设置需要查询的年月（格式: YYYY-MM）
@@ -56,7 +55,7 @@ both_df = getGeomagneticData(time, index_type=['Dst', 'AE'])
 dst_df.to_csv(f"Dst_{time}.csv", index=False)
 ```
 
-### 3. 函数接口说明
+### 2. 函数接口说明
 
 ```python
 getGeomagneticData(time, index_type='Dst')
@@ -74,7 +73,7 @@ getGeomagneticData(time, index_type='Dst')
 
 ## ⚠️ 注意事项
 
-- 数据源来自 [京都大学WDC地磁中心](https://wdc.kugi.kyoto-u.ac.jp/wdc/Sec3.html)，[GFZ](https://kp.gfz.de/)不同时间段使用不同数据接口，存在数据发布延迟。
+- 数据源来自 [京都大学WDC地磁中心](https://wdc.kugi.kyoto-u.ac.jp/wdc/Sec3.html)，[GFZ](https://kp.gfz.de/).不同时间段使用不同数据接口，存在数据发布延迟。
 - 如遇到请求失败（如返回404错误），请检查：
   - 查询时间段是否已有数据发布。
   - 查询时间是否在支持的数据范围内。
---
+++ getSolarwindData.py
@@ -0,0 +1,133 @@
+import requests
+
+import pandas as pd
+import numpy as np
+from io import StringIO
+import calendar
+from datetime import datetime
+
+from tqdm import tqdm
+import warnings
+warnings.filterwarnings("ignore")
+
+
+
+# get ACE Data
+def getACEdata_1h(year, month):
+    # https://sohoftp.nascom.nasa.gov/sdb/goes/ace/monthly/202405_ace_swepam_1h.txt
+    url1 = "https://sohoftp.nascom.nasa.gov/sdb/goes/ace/monthly/"+str(year)+str(month).zfill(2)+"_ace_swepam_1h.txt"
+    # https://sohoftp.nascom.nasa.gov/sdb/goes/ace/monthly/202405_ace_mag_1h.txt
+    url2 = "https://sohoftp.nascom.nasa.gov/sdb/goes/ace/monthly/"+str(year)+str(month).zfill(2)+"_ace_mag_1h.txt"
+
+    response1 = requests.get(url1)
+    response2 = requests.get(url2)
+
+    text1 = response1.text
+    text2 = response2.text
+
+    df1 = pd.read_csv(StringIO(text1), lineterminator='\n', skiprows=range(0, 18), header=None, delim_whitespace=True)
+    df2 = pd.read_csv(StringIO(text2), lineterminator='\n', skiprows=range(0, 20), header=None, delim_whitespace=True)
+    #                Modified Seconds   -------------  Solar Wind  -----------
+    # UT Date   Time  Julian  of the          Proton      Bulk         Ion
+    # YR MO DA  HHMM    Day     Day     S    Density     Speed     Temperature
+    #-------------------------------------------------------------------------
+    df1.columns = ['year', 'month', 'day', 'HHMM', 'ModifiedJulianDay', 'SecondsoftheDay', 'S1', 'ProtonDensity', 'BulkSpeed', 'IonTemperature']
+    #                 Modified Seconds
+    # UT Date   Time  Julian   of the   ----------------  GSM Coordinates ---------------
+    # YR MO DA  HHMM    Day      Day    S     Bx      By      Bz      Bt     Lat.   Long.
+    #------------------------------------------------------------------------------------
+    df2.columns = ['year', 'month', 'day', 'HHMM', 'ModifiedJulianDay', 'SecondsoftheDay', 'S2', 'Bx', 'By', 'Bz', 'Bt', 'Lat', 'Long']
+    
+    df = pd.merge(df1, df2, on=['year', 'month', 'day', 'HHMM', 'ModifiedJulianDay', 'SecondsoftheDay'])
+    
+    df["month"] = df["month"].astype(str).str.zfill(2)
+    df["day"] = df["day"].astype(str).str.zfill(2)
+    df["HHMM"] = df["HHMM"].astype(str).str.zfill(4)
+    df["Time"] = pd.to_datetime(df[['year', 'month', 'day', 'HHMM']].astype(str).agg(''.join, axis=1), format='%Y%m%d%H%M')
+    df = df.drop(columns=['year', 'month', 'day', 'HHMM', 'ModifiedJulianDay', 'SecondsoftheDay'])
+    df = df[['Time', 'S1','ProtonDensity', 'BulkSpeed', 'IonTemperature', 'S2', 'Bx', 'By', 'Bz', 'Bt', 'Lat', 'Long']]
+    
+    return df
+
+def getACEdata_1m(year, month, day):
+    # https://sohoftp.nascom.nasa.gov/sdb/goes/ace/daily/20010807_ace_swepam_1m.txt
+    url1 = "https://sohoftp.nascom.nasa.gov/sdb/goes/ace/daily/"+str(year)+str(month).zfill(2)+str(day).zfill(2)+"_ace_swepam_1m.txt"
+    # https://sohoftp.nascom.nasa.gov/sdb/goes/ace/daily/20010807_ace_mag_1m.txt
+    url2 = "https://sohoftp.nascom.nasa.gov/sdb/goes/ace/daily/"+str(year)+str(month).zfill(2)+str(day).zfill(2)+"_ace_mag_1m.txt"
+
+    response1 = requests.get(url1)
+    response2 = requests.get(url2)
+
+    text1 = response1.text
+    text2 = response2.text
+
+    df1 = pd.read_csv(StringIO(text1), lineterminator='\n', skiprows=range(0, 18), header=None, delim_whitespace=True)
+    df2 = pd.read_csv(StringIO(text2), lineterminator='\n', skiprows=range(0, 20), header=None, delim_whitespace=True)
+    #                Modified Seconds   -------------  Solar Wind  -----------
+    # UT Date   Time  Julian  of the          Proton      Bulk         Ion
+    # YR MO DA  HHMM    Day     Day     S    Density     Speed     Temperature
+    #-------------------------------------------------------------------------
+    df1.columns = ['year', 'month', 'day', 'HHMM', 'ModifiedJulianDay', 'SecondsoftheDay', 'S1', 'ProtonDensity', 'BulkSpeed', 'IonTemperature']
+    #                 Modified Seconds
+    # UT Date   Time  Julian   of the   ----------------  GSM Coordinates ---------------
+    # YR MO DA  HHMM    Day      Day    S     Bx      By      Bz      Bt     Lat.   Long.
+    #------------------------------------------------------------------------------------
+    df2.columns = ['year', 'month', 'day', 'HHMM', 'ModifiedJulianDay', 'SecondsoftheDay', 'S2', 'Bx', 'By', 'Bz', 'Bt', 'Lat', 'Long']
+    
+    df = pd.merge(df1, df2, on=['year', 'month', 'day', 'HHMM', 'ModifiedJulianDay', 'SecondsoftheDay'])
+    
+    df["month"] = df["month"].astype(str).str.zfill(2)
+    df["day"] = df["day"].astype(str).str.zfill(2)
+    df["HHMM"] = df["HHMM"].astype(str).str.zfill(4)
+    df["Time"] = pd.to_datetime(df[['year', 'month', 'day', 'HHMM']].astype(str).agg(''.join, axis=1), format='%Y%m%d%H%M')
+    df = df.drop(columns=['year', 'month', 'day', 'HHMM', 'ModifiedJulianDay', 'SecondsoftheDay'])
+    df = df[['Time', 'S1','ProtonDensity', 'BulkSpeed', 'IonTemperature', 'S2', 'Bx', 'By', 'Bz', 'Bt', 'Lat', 'Long']]
+    
+    return df
+
+def getSolarwindData(time, mission='ACE', interval='1h'):
+    """
+    Get solar wind data from ACE spacecraft.
+    
+    Parameters:
+    time (str): Time in the format 'YYYY-MM-DD HH:MM:SS'.
+    start (str): Start time in the format 'YYYY-MM-DD HH:MM:SS'.
+    end (str): End time in the format 'YYYY-MM-DD HH:MM:SS'.
+    interval (str): Interval for data retrieval ('1h' or '1m'). Default is '1h'.
+    
+    Returns:
+    pd.DataFrame: DataFrame containing solar wind data.
+    """
+    
+    time = pd.to_datetime(time)
+    year = time.year
+    month = time.month
+    if mission == 'ACE':
+        if interval == '1h':
+            df = getACEdata_1h(year, month)
+        elif interval == '1m':
+            df = pd.DataFrame()
+            for day in range(1, calendar.monthrange(year, month)[1] + 1):
+                # Get data for each day in the month
+                try:
+                    df_day = getACEdata_1m(year, month, day)
+                    df = pd.concat([df, df_day], ignore_index=True)
+                except Exception as e:
+                    print(f"Error retrieving data for {year}-{month:02d}-{day:02d}: {e}")
+                    
+            df.sort_values(by='Time', inplace=True)
+
+        else:
+            raise ValueError("Invalid interval. Use '1h' or '1m'.")
+    else:
+        raise ValueError("Invalid mission. Use 'ACE'.")
+
+    
+    return df
+
+if __name__ == "__main__":
+    time = "2023-12"
+    mission = "ACE"
+    interval = "1m"
+    df = getSolarwindData(time, mission, interval)
+    df.to_csv(f"{mission}_{time}_{interval}.csv", index=False)
